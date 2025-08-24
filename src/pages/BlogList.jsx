556d84c0a2f55f64ac8fa4c76a3546ee56cdd3d9
Rukhwith8888 <rukhwith.8888@gmail.com>
2025-08-25 03:49:47 +0530

    refracted the blogapp using redux for post management

--- src/pages/BlogList.jsx
+++ src/pages/BlogList.jsx
@@ -1,16 +1,75 @@
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { deletePost, addPost, updatePost } from "../redux/posts";
+import { useState } from "react";
+
 
 export default function BlogList() {
-  const posts = useSelector(state => state.posts);
+  const posts = useSelector((state) => state.posts);
+  const dispatch = useDispatch();
+
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [editingId, setEditingId] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (editingId) {
+      dispatch(updatePost({ id: editingId, title, content }));
+      setEditingId(null);
+    } else {
+      dispatch(
+        addPost({
+          id: posts.length + 1,
+          title,
+          content,
+        })
+      );
+    }
+    setTitle("");
+    setContent("");
+  };
+
+  const handleEdit = (post) => {
+    setEditingId(post.id);
+    setTitle(post.title);
+    setContent(post.content);
+  };
+
   return (
     <div className="p">
       <h2>My Blog List</h2>
-      {posts.map(post => (
+      
+      <form onSubmit={handleSubmit}>
+        <input
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Title"
+          required
+        />
+        <textarea
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          placeholder="Content"
+          required
+        />
+        <button type="submit">{editingId ? "Update Post" : "Add Post"}</button>
+      </form>
+
+      {posts.map((post) => (
         <div key={post.id} className="post">
           <h3>{post.title}</h3>
-          <p>{post.name}</p>
-          <Link to={`/blog/${post.id}`} style={{textDecoration:"none"}}>Read More....</Link>
+          <p>{post.content}</p>
+          <Link
+            to={`/blog/${post.id}`}
+            style={{ textDecoration: "none", marginRight: "10px" }}
+          >
+            Read More...
+          </Link>
+          <div>
+          <button onClick={() => handleEdit(post)}>Edit</button>
+          <button onClick={() => dispatch(deletePost(post.id))}>Delete</button>
+          </div>
         </div>
       ))}
     </div>
--- src/redux/posts.jsx
+++ src/redux/posts.jsx
@@ -1,12 +1,31 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = [
-  { id: 1, title: "High School", name: "Oxford High School.", content: "Hey, I studied 8th, 9th and 10th here." },
-  { id: 2, title: "Intermediate", name: "Sri Chaitanya Jr College.", content: "I have Chosen M.P.C course here" }
+  { id: 1, title: "High School",content: "Oxford High School. I studied 8th, 9th and 10th here." },
+  { id: 2, title: "Intermediate",content: "Sri Chaitanya Jr College.I have Chosen M.P.C course here" }
 ];
 
 const postsSlice = createSlice({
   name: "posts",
   initialState,
-  reducers: {}});
+  reducers: {
+    addPost: (state, action) => {
+      state.push(action.payload);
+    },
+    updatePost: (state, action) => {
+      const { id, title, content } = action.payload;
+      const post = state.find(p => p.id === id);
+      if (post) {
+        post.title = title;
+        post.content = content;
+      }
+    },
+    deletePost: (state, action) => {
+      return state.filter(p => p.id !== action.payload);
+    }
+  }
+});
+
+export const { addPost, updatePost, deletePost } = postsSlice.actions;
 export default postsSlice.reducer;
+
